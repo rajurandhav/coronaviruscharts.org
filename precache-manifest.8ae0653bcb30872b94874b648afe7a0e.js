ef73edcddcf02ec84b5ee55c5a0a8b419dbfc14a
Mayank <mayanksaxena001@gmail.com>
2020-04-05 23:22:14 +0530

    Updates

---
+++ precache-manifest.8ae0653bcb30872b94874b648afe7a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0b5b03bda4b75ecda8188caf7dd27b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d75fd6244e6d543afa",
+    "url": "/static/css/main.0abe6389.chunk.css"
+  },
+  {
+    "revision": "168c3d1e7f86e597c9bb",
+    "url": "/static/js/2.41f75ee9.chunk.js"
+  },
+  {
+    "revision": "0f87e0cfbb145332e9889452e79883f4",
+    "url": "/static/js/2.41f75ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d75fd6244e6d543afa",
+    "url": "/static/js/main.7064037c.chunk.js"
+  },
+  {
+    "revision": "ec491b8d855c79545d6d",
+    "url": "/static/js/runtime-main.7db71481.js"
+  }
+]);
